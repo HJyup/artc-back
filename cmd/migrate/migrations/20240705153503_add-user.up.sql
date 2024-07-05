3921504d8867b7a1aa4f42912bc2c42bb2b99674
HJK <89708817+HJyup@users.noreply.github.com>
2024-07-05 16:54:49 +0100

    feat: add user model (#8)

---
+++ cmd/migrate/migrations/20240705153503_add-user.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    `id`          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `avatar`      VARCHAR(255) NOT NULL,
+    `first_name`  VARCHAR(255) NOT NULL,
+    `last_name`   VARCHAR(255) NOT NULL,
+    `email`       VARCHAR(255) NOT NULL,
+    `password`    VARCHAR(255) NOT NULL,
+    `speciality_id` INT UNSIGNED NOT NULL,
+    `location`    VARCHAR(255) NOT NULL,
+    `is_accepted` BOOLEAN NOT NULL DEFAULT FALSE,
+    `is_reviewer` BOOLEAN NOT NULL DEFAULT FALSE,
+    `createdAt`   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`email`),
+    FOREIGN KEY (`speciality_id`) REFERENCES specialities(`id`)
+);
