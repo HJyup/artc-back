85545d0e36b0d5d3216f01b0c7141a24b0203143
HJK <89708817+HJyup@users.noreply.github.com>
2024-07-06 16:59:35 +0100

    feat: add user store and uuid (#9)

--- cmd/migrate/migrations/20240705153503_add-user.up.sql
+++ cmd/migrate/migrations/20240705153503_add-user.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    `id`          INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `avatar`      VARCHAR(255) NOT NULL,
+    `pk`          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id`          VARCHAR(255) NOT NULL,
+    `avatar`      VARCHAR(255),
     `first_name`  VARCHAR(255) NOT NULL,
     `last_name`   VARCHAR(255) NOT NULL,
     `email`       VARCHAR(255) NOT NULL,
@@ -12,7 +13,7 @@ CREATE TABLE IF NOT EXISTS users
     `is_reviewer` BOOLEAN NOT NULL DEFAULT FALSE,
     `createdAt`   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (`id`),
+    PRIMARY KEY (`pk`),
     UNIQUE KEY (`email`),
     FOREIGN KEY (`speciality_id`) REFERENCES specialities(`id`)
 );
---
+++ types/speciality.go
@@ -0,0 +1,12 @@
+package types
+
+const (
+	Musician     Speciality = "musician"
+	Actor        Speciality = "actor"
+	VisualArtist Speciality = "visual_artist"
+	Writer       Speciality = "writer"
+	Designer     Speciality = "designer"
+	Dancer       Speciality = "dancer"
+	Photographer Speciality = "photographer"
+	Filmmaker    Speciality = "filmmaker"
+)
