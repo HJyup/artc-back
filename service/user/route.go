87fc7ed6698c5afe6cf6ad96c213e3d5a0149068
danyilbutov <danyil.butov@solidgate.com>
2024-07-06 17:47:04 +0100

    feat: add user handlers and jwt support

--- service/user/route.go
+++ service/user/route.go
@@ -1,15 +1,22 @@
 package user
 
 import (
+	"artc-back/config"
+	"artc-back/service/auth"
+	"artc-back/types"
+	"artc-back/utils"
+	"errors"
+	"fmt"
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 type Handler struct {
-	store any
+	store types.UserStore
 }
 
-func NewHandler(store any) *Handler {
+func NewHandler(store types.UserStore) *Handler {
 	return &Handler{store: store}
 }
 
@@ -18,10 +25,87 @@ func (handler *Handler) RegisterRouters(router *mux.Router) {
 	router.HandleFunc("/register", handler.HandleRegister).Methods("POST")
 }
 
-func (handler *Handler) HandleLogin(_ http.ResponseWriter, _ *http.Request) {
-	// handle login
+func (handler *Handler) HandleLogin(w http.ResponseWriter, r *http.Request) {
+	var payload types.LoginUserPayload
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := utils.Validate.Struct(payload); err != nil {
+		var errValidation validator.ValidationErrors
+		errors.As(err, &errValidation)
+		utils.WriteError(w, http.StatusBadRequest, errValidation)
+	}
+
+	user, err := handler.store.GetUserByEmail(payload.Email)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("not found, invalid email or password"))
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if !auth.ComparePassword(user.Password, []byte(payload.Password)) {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("not found, invalid email or password"))
+		return
+	}
+
+	secret := []byte(config.Envs.JWTSecret)
+	token, err := auth.CreateJWT(secret, user.ID)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, fmt.Errorf("error creating jwt token"))
+		return
+
+	}
+
+	err = utils.WriteJSON(w, http.StatusCreated, map[string]string{"token": token})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
 }
 
-func (handler *Handler) HandleRegister(_ http.ResponseWriter, _ *http.Request) {
-	// handle register
+func (handler *Handler) HandleRegister(w http.ResponseWriter, r *http.Request) {
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := utils.Validate.Struct(payload); err != nil {
+		var errValidation validator.ValidationErrors
+		errors.As(err, &errValidation)
+		utils.WriteError(w, http.StatusBadRequest, errValidation)
+	}
+
+	_, err := handler.store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", payload.Email))
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	err = handler.store.CreateUser(types.User{
+		FirstName:    payload.FirstName,
+		SecondName:   payload.SecondName,
+		Email:        payload.Email,
+		Password:     hashedPassword,
+		Location:     payload.Location,
+		SpecialityID: payload.SpecialityID,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	err = utils.WriteJSON(w, http.StatusCreated, nil)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
 }
