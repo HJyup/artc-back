f6f672dee1d783a9653673a49ae09c35d862807a
danyilbutov <danyil.butov@solidgate.com>
2024-07-06 16:52:58 +0100

    feat: add user store and uuid

--- service/user/store.go
+++ service/user/store.go
@@ -1,7 +1,10 @@
 package user
 
 import (
+	"artc-back/types"
 	"database/sql"
+	"fmt"
+	"os/exec"
 )
 
 type Store struct {
@@ -10,4 +13,99 @@ type Store struct {
 
 func NewStore(db *sql.DB) *Store {
 	return &Store{db}
+}
+
+func (store *Store) CreateUser(user types.User) error {
+	uuid, err := exec.Command("uuidgen").Output()
+	if err != nil {
+		return err
+	}
+	_, err = store.db.Exec("INSERT INTO users (id, first_name, last_name, email, password, speciality_id, location) VALUES (?, ?, ?, ?, ?, ?, ?)",
+		uuid, user.FirstName, user.SecondName, user.Email, user.Password, user.SpecialityID, user.Location)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (store *Store) GetUserById(id string) (*types.User, error) {
+	rows, err := store.db.Query("SELECT * FROM users WHERE id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	user := new(types.User)
+	for rows.Next() {
+		user, err = scanRowIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if user == nil || user.PK == 0 {
+		return nil, fmt.Errorf("error getting user by id: %s", id)
+	}
+
+	return convertToResponseUser(user), nil
+}
+
+func (store *Store) GetUserByEmail(email string) (*types.User, error) {
+	rows, err := store.db.Query("SELECT * FROM users WHERE email = ?", email)
+	if err != nil {
+		return nil, err
+	}
+
+	user := new(types.User)
+	for rows.Next() {
+		user, err = scanRowIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if user == nil || user.PK == 0 {
+		return nil, fmt.Errorf("error getting user by email: %s", email)
+	}
+
+	return convertToResponseUser(user), nil
+}
+
+func scanRowIntoUser(rows *sql.Rows) (*types.User, error) {
+	user := new(types.User)
+	err := rows.Scan(
+		&user.PK,
+		&user.ID,
+		&user.Avatar,
+		&user.FirstName,
+		&user.SecondName,
+		&user.Email,
+		&user.Password,
+		&user.SpecialityID,
+		&user.Location,
+		&user.IsAccepted,
+		&user.IsReviewer,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func convertToResponseUser(user *types.User) *types.User {
+	return &types.User{
+		ID:           user.ID,
+		Avatar:       user.Avatar,
+		FirstName:    user.FirstName,
+		SecondName:   user.SecondName,
+		Email:        user.Email,
+		Password:     user.Password,
+		SpecialityID: user.SpecialityID,
+		Location:     user.Location,
+		IsAccepted:   user.IsAccepted,
+		IsReviewer:   user.IsReviewer,
+		CreatedAt:    user.CreatedAt,
+	}
 }
--- types/user.go
+++ types/user.go
@@ -1,41 +1,41 @@
 package types
 
-import "time"
+import (
+	"github.com/google/uuid"
+	"time"
+)
 
 type Speciality string
 
-const (
-	Musician     Speciality = "musician"
-	Actor        Speciality = "actor"
-	VisualArtist Speciality = "visual_artist"
-	Writer       Speciality = "writer"
-	Designer     Speciality = "designer"
-	Dancer       Speciality = "dancer"
-	Photographer Speciality = "photographer"
-	Filmmaker    Speciality = "filmmaker"
-)
+type UserStore interface {
+	GetUserByEmail(email string) (*User, error)
+	GetUserById(id string) (*User, error)
+	CreateUser(user User) error
+}
 
 type User struct {
-	ID         int        `json:"id"`
-	Avatar     string     `json:"avatar"`
-	FirstName  string     `json:"first_name"`
-	SecondName string     `json:"second_name"`
-	Email      string     `json:"email"`
-	Password   string     `json:"password"`
-	Speciality Speciality `json:"speciality"`
-	Location   string     `json:"location"`
-	IsAccepted bool       `json:"is_accepted"`
-	IsReviewer bool       `json:"is_reviewer"`
-	CreatedAt  time.Time  `json:"created_at"`
+	PK           int       `json:"pk"`
+	ID           uuid.UUID `json:"id"`
+	Avatar       string    `json:"avatar"`
+	FirstName    string    `json:"first_name"`
+	SecondName   string    `json:"second_name"`
+	Email        string    `json:"email"`
+	Password     string    `json:"password"`
+	SpecialityID int       `json:"speciality_id"`
+	Location     string    `json:"location"`
+	IsAccepted   bool      `json:"is_accepted"`
+	IsReviewer   bool      `json:"is_reviewer"`
+	CreatedAt    time.Time `json:"created_at"`
 }
 
 type RegisterUserPayload struct {
-	FirstName  string `json:"first_name" validate:"required"`
-	SecondName string `json:"second_name" validate:"required"`
-	Email      string `json:"email" validate:"required,email"`
-	Password   string `json:"password" validate:"required,min=6,max=32"`
-	Speciality string `json:"speciality" validate:"required"`
-	Location   string `json:"location" validate:"required"`
+	ID         uuid.UUID `json:"id"`
+	FirstName  string    `json:"first_name" validate:"required"`
+	SecondName string    `json:"second_name" validate:"required"`
+	Email      string    `json:"email" validate:"required,email"`
+	Password   string    `json:"password" validate:"required,min=6,max=32"`
+	Speciality string    `json:"speciality" validate:"required"`
+	Location   string    `json:"location" validate:"required"`
 }
 
 type LoginUserPayload struct {
